67cab47f34898df5455a58d65aa66237cd6b626e
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2025-09-03 15:39:49 +0800

    added sig

--- media/lua/client/AdminRadZone_Symbol.lua
+++ media/lua/client/AdminRadZone_Symbol.lua
@@ -1,3 +1,104 @@
+
+----------------------------------------------------------------
+-----  ▄▄▄   ▄    ▄   ▄  ▄▄▄▄▄   ▄▄▄   ▄   ▄   ▄▄▄    ▄▄▄  -----
+----- █   ▀  █    █▄▄▄█    █    █   ▀  █▄▄▄█  ▀  ▄█  █ ▄▄▀ -----
+----- █  ▀█  █      █      █    █   ▄  █   █  ▄   █  █   █ -----
+-----  ▀▀▀▀  ▀▀▀▀   ▀      ▀     ▀▀▀   ▀   ▀   ▀▀▀   ▀   ▀ -----
+----------------------------------------------------------------
+--                                                            --
+--   Project Zomboid Modding Commissions                      --
+--   https://steamcommunity.com/id/glytch3r/myworkshopfiles   --
+--                                                            --
+--   ▫ Discord  ꞉   glytch3r                                  --
+--   ▫ Support  ꞉   https://ko-fi.com/glytch3r                --
+--   ▫ Youtube  ꞉   https://www.youtube.com/@glytch3r         --
+--   ▫ Github   ꞉   https://github.com/Glytch3r               --
+--                                                            --
+----------------------------------------------------------------
+----- ▄   ▄   ▄▄▄   ▄   ▄   ▄▄▄     ▄      ▄   ▄▄▄▄  ▄▄▄▄  -----
+----- █   █  █   ▀  █   █  ▀   █    █      █      █  █▄  █ -----
+----- ▄▀▀ █  █▀  ▄  █▀▀▀█  ▄   █    █    █▀▀▀█    █  ▄   █ -----
+-----  ▀▀▀    ▀▀▀   ▀   ▀   ▀▀▀   ▀▀▀▀▀  ▀   ▀    ▀   ▀▀▀  -----
+----------------------------------------------------------------
+AdminRadZone = AdminRadZone or {}
+function AdminRadZone.hookInit()
+    AdminRadZone.Hook_ISWorldMapRender = AdminRadZone.Hook_ISWorldMapRender or ISWorldMap.render
+    AdminRadZone.Hook_ISMiniMapOuterRender = AdminRadZone.Hook_ISMiniMapOuterRender or ISMiniMapOuter.render
+
+    function ISWorldMap:render()
+        AdminRadZone.Hook_ISWorldMapRender(self)
+        local data = AdminRadZoneData
+        if data and data.state and data.x and data.y and data.rad and data.run then
+            AdminRadZone.renderSym(self.mapAPI, data.x, data.y, data.rad, 1, 0, 0, 1, self, nil, nil, nil, 3)
+        end
+    end
+
+    function ISMiniMapOuter:render()
+        AdminRadZone.Hook_ISMiniMapOuterRender(self)
+        local data = AdminRadZoneData
+
+        if data and data.state and data.x and data.y and data.rad and data.run then
+            AdminRadZone.renderSym(self.inner.mapAPI, data.x, data.y, data.rad, 1, 0, 0, 1, self.inner, nil, nil, nil, 3)
+        end
+    end
+end
+Events.OnCreatePlayer.Add(AdminRadZone.hookInit)
+
+function AdminRadZone.renderSym(mapAPI, posX, posY, radius, r, g, b, alpha, drawTarget, uiXOffset, uiYOffset, mask, thickness)
+    thickness = thickness or 2
+    local shrink = math.pi / 27
+    local screenCenterX = mapAPI:worldToUIX(posX, posY)
+    local screenCenterY = mapAPI:worldToUIY(posX, posY)
+
+    if uiXOffset then
+        screenCenterX = screenCenterX + uiXOffset
+        screenCenterY = screenCenterY + uiYOffset
+    end
+
+    local screenRadius = math.abs(mapAPI:worldToUIX(posX + radius, posY) - screenCenterX)
+    local isShow = SandboxVars.AdminRadZone.MapSymbols
+
+    for deg = 0, math.pi * 2, shrink do
+        local xScreen1 = screenCenterX + screenRadius * math.cos(deg)
+        local yScreen1 = screenCenterY + screenRadius * math.sin(deg)
+        local xScreen2 = screenCenterX + screenRadius * math.cos(deg + shrink)
+        local yScreen2 = screenCenterY + screenRadius * math.sin(deg + shrink)
+
+        local hide = false
+        if mask then
+            hide, xScreen1, yScreen1, xScreen2, yScreen2 = AdminRadZone.mask(xScreen1, yScreen1, xScreen2, yScreen2, mask)
+        end
+        if not hide and drawTarget and isShow then
+            local halfThickness = thickness / 2
+        
+            drawTarget:drawRect(xScreen1 - halfThickness, yScreen1 - halfThickness, thickness, thickness, alpha, r, g, b)
+            drawTarget:drawRect(xScreen2 - halfThickness, yScreen2 - halfThickness, thickness, thickness, alpha, r, g, b)
+        end
+    end
+end
+
+function AdminRadZone.mask(xScreen1, yScreen1, xScreen2, yScreen2, mask)
+    if not mask then
+        return false, xScreen1, yScreen1, xScreen2, yScreen2
+    end
+    local hide = false
+    if (xScreen1 < mask.x1 and xScreen2 < mask.x1) or
+       (xScreen1 > mask.x2 and xScreen2 > mask.x2) or
+       (yScreen1 < mask.y1 and yScreen2 < mask.y1) or
+       (yScreen1 > mask.y2 and yScreen2 > mask.y2) then
+        hide = true
+        return hide, xScreen1, yScreen1, xScreen2, yScreen2
+    end
+    if xScreen1 < mask.x1 then xScreen1 = mask.x1 end
+    if xScreen1 > mask.x2 then xScreen1 = mask.x2 end
+    if yScreen1 < mask.y1 then yScreen1 = mask.y1 end
+    if yScreen1 > mask.y2 then yScreen1 = mask.y2 end
+    if xScreen2 < mask.x1 then xScreen2 = mask.x1 end
+    if xScreen2 > mask.x2 then xScreen2 = mask.x2 end
+    if yScreen2 < mask.y1 then yScreen2 = mask.y1 end
+    if yScreen2 > mask.y2 then yScreen2 = mask.y2 end
+    return hide, xScreen1, yScreen1, xScreen2, yScreen2
+end
 --[[ AdminRadZone = AdminRadZone or {}
 function AdminRadZone.hookInit()
     AdminRadZone.Hook_ISWorldMapRender = AdminRadZone.Hook_ISWorldMapRender or ISWorldMap.render
@@ -96,82 +197,3 @@ function AdminRadZone.symHandler()
 end
 Events.OnPostRender.Add(AdminRadZone.symHandler)
  ]]
-AdminRadZone = AdminRadZone or {}
-function AdminRadZone.hookInit()
-    AdminRadZone.Hook_ISWorldMapRender = AdminRadZone.Hook_ISWorldMapRender or ISWorldMap.render
-    AdminRadZone.Hook_ISMiniMapOuterRender = AdminRadZone.Hook_ISMiniMapOuterRender or ISMiniMapOuter.render
-
-    function ISWorldMap:render()
-        AdminRadZone.Hook_ISWorldMapRender(self)
-        local data = AdminRadZoneData
-        if data and data.state and data.x and data.y and data.rad and data.run then
-            AdminRadZone.renderSym(self.mapAPI, data.x, data.y, data.rad, 1, 0, 0, 1, self, nil, nil, nil, 3)
-        end
-    end
-
-    function ISMiniMapOuter:render()
-        AdminRadZone.Hook_ISMiniMapOuterRender(self)
-        local data = AdminRadZoneData
-
-        if data and data.state and data.x and data.y and data.rad and data.run then
-            AdminRadZone.renderSym(self.inner.mapAPI, data.x, data.y, data.rad, 1, 0, 0, 1, self.inner, nil, nil, nil, 3)
-        end
-    end
-end
-Events.OnCreatePlayer.Add(AdminRadZone.hookInit)
-
-function AdminRadZone.renderSym(mapAPI, posX, posY, radius, r, g, b, alpha, drawTarget, uiXOffset, uiYOffset, mask, thickness)
-    thickness = thickness or 2
-    local shrink = math.pi / 27
-    local screenCenterX = mapAPI:worldToUIX(posX, posY)
-    local screenCenterY = mapAPI:worldToUIY(posX, posY)
-
-    if uiXOffset then
-        screenCenterX = screenCenterX + uiXOffset
-        screenCenterY = screenCenterY + uiYOffset
-    end
-
-    local screenRadius = math.abs(mapAPI:worldToUIX(posX + radius, posY) - screenCenterX)
-    local isShow = SandboxVars.AdminRadZone.MapSymbols
-
-    for deg = 0, math.pi * 2, shrink do
-        local xScreen1 = screenCenterX + screenRadius * math.cos(deg)
-        local yScreen1 = screenCenterY + screenRadius * math.sin(deg)
-        local xScreen2 = screenCenterX + screenRadius * math.cos(deg + shrink)
-        local yScreen2 = screenCenterY + screenRadius * math.sin(deg + shrink)
-
-        local hide = false
-        if mask then
-            hide, xScreen1, yScreen1, xScreen2, yScreen2 = AdminRadZone.mask(xScreen1, yScreen1, xScreen2, yScreen2, mask)
-        end
-        if not hide and drawTarget and isShow then
-            local halfThickness = thickness / 2
-        
-            drawTarget:drawRect(xScreen1 - halfThickness, yScreen1 - halfThickness, thickness, thickness, alpha, r, g, b)
-            drawTarget:drawRect(xScreen2 - halfThickness, yScreen2 - halfThickness, thickness, thickness, alpha, r, g, b)
-        end
-    end
-end
-
-function AdminRadZone.mask(xScreen1, yScreen1, xScreen2, yScreen2, mask)
-    if not mask then
-        return false, xScreen1, yScreen1, xScreen2, yScreen2
-    end
-    local hide = false
-    if (xScreen1 < mask.x1 and xScreen2 < mask.x1) or
-       (xScreen1 > mask.x2 and xScreen2 > mask.x2) or
-       (yScreen1 < mask.y1 and yScreen2 < mask.y1) or
-       (yScreen1 > mask.y2 and yScreen2 > mask.y2) then
-        hide = true
-        return hide, xScreen1, yScreen1, xScreen2, yScreen2
-    end
-    if xScreen1 < mask.x1 then xScreen1 = mask.x1 end
-    if xScreen1 > mask.x2 then xScreen1 = mask.x2 end
-    if yScreen1 < mask.y1 then yScreen1 = mask.y1 end
-    if yScreen1 > mask.y2 then yScreen1 = mask.y2 end
-    if xScreen2 < mask.x1 then xScreen2 = mask.x1 end
-    if xScreen2 > mask.x2 then xScreen2 = mask.x2 end
-    if yScreen2 < mask.y1 then yScreen2 = mask.y1 end
-    if yScreen2 > mask.y2 then yScreen2 = mask.y2 end
-    return hide, xScreen1, yScreen1, xScreen2, yScreen2
-end
--- media/lua/shared/Translate/AdminRadZone_Image.lua
+++ media/lua/shared/Translate/AdminRadZone_Image.lua
@@ -1,3 +1,26 @@
+----------------------------------------------------------------
+-----  ▄▄▄   ▄    ▄   ▄  ▄▄▄▄▄   ▄▄▄   ▄   ▄   ▄▄▄    ▄▄▄  -----
+----- █   ▀  █    █▄▄▄█    █    █   ▀  █▄▄▄█  ▀  ▄█  █ ▄▄▀ -----
+----- █  ▀█  █      █      █    █   ▄  █   █  ▄   █  █   █ -----
+-----  ▀▀▀▀  ▀▀▀▀   ▀      ▀     ▀▀▀   ▀   ▀   ▀▀▀   ▀   ▀ -----
+----------------------------------------------------------------
+--                                                            --
+--   Project Zomboid Modding Commissions                      --
+--   https://steamcommunity.com/id/glytch3r/myworkshopfiles   --
+--                                                            --
+--   ▫ Discord  ꞉   glytch3r                                  --
+--   ▫ Support  ꞉   https://ko-fi.com/glytch3r                --
+--   ▫ Youtube  ꞉   https://www.youtube.com/@glytch3r         --
+--   ▫ Github   ꞉   https://github.com/Glytch3r               --
+--                                                            --
+----------------------------------------------------------------
+----- ▄   ▄   ▄▄▄   ▄   ▄   ▄▄▄     ▄      ▄   ▄▄▄▄  ▄▄▄▄  -----
+----- █   █  █   ▀  █   █  ▀   █    █      █      █  █▄  █ -----
+----- ▄▀▀ █  █▀  ▄  █▀▀▀█  ▄   █    █    █▀▀▀█    █  ▄   █ -----
+-----  ▀▀▀    ▀▀▀   ▀   ▀   ▀▀▀   ▀▀▀▀▀  ▀   ▀    ▀   ▀▀▀  -----
+----------------------------------------------------------------
+
+
 MapSymbolDefinitions.getInstance():addTexture("AdminRadZone_1", "media/ui/LootableMaps/AdminRadZone_128.png")
 MapSymbolDefinitions.getInstance():addTexture("AdminRadZone_2", "media/ui/LootableMaps/AdminRadZone_256.png")
 MapSymbolDefinitions.getInstance():addTexture("AdminRadZone_3", "media/ui/LootableMaps/AdminRadZone_512.png")
